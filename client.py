b215b024b4d4b177bf3633ca0c8cae5ca1c510b0
d28035203 <289484834+d28035203@users.noreply.github.com>
2018-03-19 21:16:55 +0530

    feat: multi-client server with broadcast

---
+++ client.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""socket-chai-chat client."""
+
+from __future__ import print_function
+import socket
+import threading
+import sys
+
+HOST = "127.0.0.1"
+PORT = 5050
+
+
+def recv_loop(sock):
+ while True:
+ data = sock.recv(1024)
+ if not data:
+ print("\n[disconnected]")
+ os_exit()
+ break
+ sys.stdout.write(data.decode("utf-8", "replace"))
+ sys.stdout.flush()
+
+
+def os_exit():
+ try:
+ sys.exit(0)
+ except SystemExit:
+ pass
+
+
+def main():
+ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ sock.connect((HOST, PORT))
+ threading.Thread(target=recv_loop, args=(sock,), daemon=True).start()
+ try:
+ while True:
+ line = sys.stdin.readline()
+ if not line:
+ break
+ sock.sendall(line.encode("utf-8"))
+ except KeyboardInterrupt:
+ pass
+ finally:
+ sock.close()
+
+
+if __name__ == "__main__":
+ main()
---
+++ server.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""socket-chai-chat server — broadcast TCP chat (CN lab 2018)."""
+
+from __future__ import print_function
+import socket
+import threading
+import sys
+
+HOST = "0.0.0.0"
+PORT = 5050
+clients = []
+lock = threading.Lock()
+
+
+def broadcast(msg, skip=None):
+ with lock:
+ dead = []
+ for c in clients:
+ if c is skip:
+ continue
+ try:
+ c.sendall(msg)
+ except Exception:
+ dead.append(c)
+ for c in dead:
+ clients.remove(c)
+
+
+def handle(conn, addr):
+ print("joined:", addr)
+ try:
+ conn.sendall(b"welcome to socket-chai-chat. type something.\n")
+ while True:
+ data = conn.recv(1024)
+ if not data:
+ break
+ line = b"[%s:%d] %s" % (addr[0].encode(), addr[1], data)
+ sys.stdout.write(line.decode("utf-8", "replace"))
+ sys.stdout.flush()
+ broadcast(line, skip=conn)
+ finally:
+ with lock:
+ if conn in clients:
+ clients.remove(conn)
+ conn.close()
+ print("left:", addr)
+
+
+def main():
+ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+ s.bind((HOST, PORT))
+ s.listen(5)
+ print("chai chat listening on %d" % PORT)
+ try:
+ while True:
+ conn, addr = s.accept()
+ with lock:
+ clients.append(conn)
+ threading.Thread(target=handle, args=(conn, addr), daemon=True).start()
+ except KeyboardInterrupt:
+ print("\nserver stopping")
+ finally:
+ s.close()
+
+
+if __name__ == "__main__":
+ main()
